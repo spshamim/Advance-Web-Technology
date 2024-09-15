0755445c6cfc4914e67a4dc54271ce7da551a4c5
spshamim <spshamim90@gmail.com>
2024-09-15 20:43:49 +0600

    NestJS_with_MongoDB

---
+++ Nest_MongoDB/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+async function bootstrap() {
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 3600000, // cookie will last for 1 hour
+      },
+    }),
+  );
+
+  app.use(cookieParser());
+
+  await app.listen(process.env.APP_LISTEN || 3001);
+}
+bootstrap();
